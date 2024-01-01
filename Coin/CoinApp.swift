7bcd051936bbbf08efc45cfc946f42b882de4098
Kirill-Streltsov <krlsrlsv@gmail.com>
2024-01-01 15:06:27 +0700

    Initial Commit

---
+++ Coin/CoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinApp.swift
+//  Coin
+//
+//  Created by Kirill Streltsov on 01.01.24.
+//
+
+import SwiftUI
+
+@main
+struct CoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Coin
+//
+//  Created by Kirill Streltsov on 01.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
